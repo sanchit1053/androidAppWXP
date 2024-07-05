d707b89b7bab7331ff7283f9e7d0355c8d5b67a8
shreyasingla <shreya.singla1610@gmail.com>
2024-07-05 17:20:38 +0530

    added LTR mode!!! Try arabic guys!

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication; // Replace with your package name
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +11,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+
+import java.util.Locale;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -42,12 +46,12 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             this.startActivity(intent);
 
-            String welcomeMessage = "Welcome, " + username + "!";
+            String welcomeMessage = R.string.welcome + username;
             textViewWelcome.setText(welcomeMessage);
             textViewWelcome.setVisibility(View.VISIBLE);
             textViewWelcome.setTextColor(ContextCompat.getColor(this, R.color.colorGreen));
         } else {
-            textViewWelcome.setText("Invalid username or password");
+            textViewWelcome.setText(R.string.invalid_username);
             textViewWelcome.setTextColor(ContextCompat.getColor(this, R.color.colorRed));
             textViewWelcome.setVisibility(View.VISIBLE);
         }
