c4349e5e48a30722949e5026cc2157c6f303987e
sanjindal <sanjindal@microsoft.com>
2024-07-05 12:05:01 +0530

    adding all files to the repo

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.example.myapplication; // Replace with your package name
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewWelcome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_login);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewWelcome = findViewById(R.id.textViewWelcome);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        String username = editTextUsername.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (username.equals("admin") && password.equals("admin@123")) {
+            Intent intent = new Intent(this, MainActivity.class);
+            this.startActivity(intent);
+
+            String welcomeMessage = "Welcome, " + username + "!";
+            textViewWelcome.setText(welcomeMessage);
+            textViewWelcome.setVisibility(View.VISIBLE);
+            textViewWelcome.setTextColor(ContextCompat.getColor(this, R.color.colorGreen));
+        } else {
+            textViewWelcome.setText("Invalid username or password");
+            textViewWelcome.setTextColor(ContextCompat.getColor(this, R.color.colorRed));
+            textViewWelcome.setVisibility(View.VISIBLE);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button Button1, Button2, Button3, Button4;
+    LoginFragment loginFragment;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView((R.layout.activity_main));
+        Button1 = findViewById(R.id.ButtonApplication1);
+        Button2 = findViewById(R.id.ButtonApplication2);
+        Button3 = findViewById(R.id.ButtonApplication3);
+        Button4 = findViewById(R.id.ButtonApplication4);
+
+        loadFragment(R.id.timerFragment, new DigitalClockFragment());
+
+
+        addFragmentToClickListener(Button1, new TipCalculator());
+        addFragmentToClickListener(Button2, new QuoteGenerator());
+        addFragmentToClickListener(Button3, new UnitConverterFragment(Color.parseColor("#00dddd")));
+        addFragmentToClickListener(Button4, new UnitConverterFragment(Color.parseColor("#dddddd")));
+    }
+
+    public void loadFragment(int layout, Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(layout, fragment).commit();
+    }
+
+    public void loadFragmentToBackStack(int layout, Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.addToBackStack(null).add(layout, fragment).commit();
+    }
+
+    public void addFragmentToClickListener(Button button, Fragment fragment) {
+        button.setOnClickListener((view) -> loadFragmentToBackStack(R.id.frameLayout, fragment));
+    }
+
+    public void successfulLogin() {
+        removeFragment(loginFragment);
+    }
+
+    public void removeFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.remove(fragment).commit();
+    }
+}
