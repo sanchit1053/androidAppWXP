0424c4a5894b650d604f6dea075dfa3d16cf4851
umeshbhukya <bhukya.umesh9@gmail.com>
2024-07-07 04:47:23 +0530

    Made all the changes for the story generator

--- app/src/main/java/com/example/myapplication/Story_popUp.java
+++ app/src/main/java/com/example/myapplication/Story_popUp.java
@@ -1,64 +1,99 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Story_popUp#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+
 public class Story_popUp extends Fragment {
+    private TextView story,name;
+    private ImageButton mic,closeButton;
+    public int i;
+    private TextToSpeech sp;
+    boolean isSpeaking = false; // Flag to track speech state
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Story_popUp() {
-        // Required empty public constructor
+    public Story_popUp(int index) {
+        i = index;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Story_popUp.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Story_popUp newInstance(String param1, String param2) {
-        Story_popUp fragment = new Story_popUp();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_story_pop_up, container, false);
+        String[] quotes = {
+                getString(R.string.story1),
+                getString(R.string.story2),
+                getString(R.string.story3),
+                getString(R.string.story4),
+                getString(R.string.story5),
+        };
+        String[] title = {
+                getString(R.string.title1),
+                getString(R.string.title2),
+                getString(R.string.title3),
+                getString(R.string.title4),
+                getString(R.string.title5),
+        };
+        View v = inflater.inflate(R.layout.fragment_story_pop_up, container, false);
+        story = v.findViewById(R.id.allstory);  // Initialize TextView
+        mic = v.findViewById(R.id.mic);
+        closeButton = v.findViewById(R.id.closeButton);
+        name= v.findViewById(R.id.title);
+        story.setText(quotes[i]);
+        name.setText(title[i]);
+
+        sp = new TextToSpeech(getActivity().getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    sp.setLanguage(Locale.UK);
+                    sp.setSpeechRate(0.8f);
+                }
+            }
+        });
+        mic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isSpeaking) {
+                    // Start speaking
+                    String toSpeak = story.getText().toString();
+                    sp.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                    isSpeaking = true;
+                } else {
+                    // Stop speaking
+                    sp.stop();
+                    isSpeaking = false;
+                }
+            }
+        });
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Close the fragment
+                sp.stop();
+                isSpeaking = false;
+                getParentFragmentManager().beginTransaction().remove(Story_popUp.this).commit();
+            }
+        });
+        return v;
     }
+
 }
